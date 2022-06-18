68d73da972f03297e668c9ce6c8457b26c0d1ca0
Julio Villanueva <wolfri2002@gmail.com>
2022-06-17 19:50:55 -0500

    Cambio en assets, detección al perder y almacenamiento de puntaje

--- js/Diccionario.js
+++ js/Diccionario.js
@@ -5,9 +5,21 @@ class Diccionario{
 		audio:"assets/audio.svg",
 		mute:"assets/audio_mute.svg",
 		mono:{
+			izq:[
+				"assets/mono/izq/mono1.svg",
+				"assets/mono/izq/mono2.svg",
+				"assets/mono/izq/mono3.svg",
+				"assets/mono/izq/mono4.svg",
+				"assets/mono/izq/mono5.svg",
+				"assets/mono/izq/mono6.svg"
+			],
 			der:[
-				"assets/mono/mono1.svg",
-				"assets/mono/mono2.svg"
+				"assets/mono/der/mono1.svg",
+				"assets/mono/der/mono2.svg",
+				"assets/mono/der/mono3.svg",
+				"assets/mono/der/mono4.svg",
+				"assets/mono/der/mono5.svg",
+				"assets/mono/der/mono6.svg"
 			]
 		},
 		tronco:"assets/tronco.svg",
--- js/app.js
+++ js/app.js
@@ -1,23 +1,26 @@
 var content_inicio=document.getElementById('content-inicio');
-var puntajes=document.getElementById('puntajes');
+var content_puntaje_obtenido=document.getElementById('content-puntaje-obtenido');
+var puntaje_obtenido=document.getElementById('content-puntaje');
+var content_puntaje=document.getElementById('puntajes');
 var content_juego=document.getElementById('content-juego');
 var content_vidas=document.getElementById('content-vidas');
 var bananas=document.getElementById('bananas');
+var caja_nombre=document.getElementById('caja-nombre');
+var btn_aceptar=document.getElementById('btn-aceptar');
 var btn_jugar=document.getElementById('btn-jugar');
 var btn_audio=document.getElementById('btn-audio');
 var audio=document.getElementById('audio');
 var mono={
 	obj:document.getElementById('mono'),
-	vidas:5,
-	bananas:0,
-	mira:"der",
-	img:0
+	vidas:null,
+	bananas:null,
+	mira:null,
+	img:null
 }
 
 // Métodos a ejecutar al inicio
 document.addEventListener("DOMContentLoaded",()=>{
 	this.audio.loop=true;
-	this.restear();
 	this.inicio();
 });
 
@@ -33,6 +36,14 @@ btn_audio.addEventListener("click",()=>{
 });
 btn_jugar.addEventListener("click",()=>{
 	this.jugar();
+});
+btn_aceptar.addEventListener("click",()=>{
+	let datos={
+		bananas: this.mono.bananas,
+		nombre: this.caja_nombre.value
+	}
+	Puntaje.agregar(datos);
+	this.inicio();
 });
 
 // Detectar tecla para mover el mono
@@ -51,11 +62,13 @@ document.addEventListener("keydown",(evt)=>{
 		return 0;
 	}
 	if(tecla==Diccionario.tecla.izq){
-		this.mono.obj.style.left=(posi_actual-15)+"px";
+		this.mono.obj.style.left=(posi_actual-10)+"px";
+		this.mono.mira="izq";
 		camino=true;
 	}else
 	if(tecla==Diccionario.tecla.der){
-		this.mono.obj.style.left=(posi_actual+15)+"px";
+		this.mono.obj.style.left=(posi_actual+10)+"px";
+		this.mono.mira="der";
 		camino=true;
 	}
 	if(!camino){
@@ -75,16 +88,18 @@ function modal(content,visible=-1){
 						(visible)?"":"none";
 }
 
-function restear(){
+function resetear(){
 	this.mono.obj.style.bottom="0px";
 	this.mono.obj.style.left="10px";
-	this.mono.vidas=5;
+	this.mono.vidas=1;
 	this.mono.bananas=0;
 	this.mono.mira="der";
 	this.mono.img=0;
+	this.caja_nombre.value="";
 	this.bananas.innerHTML=`Bananas: ${this.mono.bananas}`;
 	// Agregar imagenes principales
 	this.mono.obj.setAttribute("src",Diccionario.imagenes.mono[this.mono.mira][this.mono.img]);
+	this.content_vidas.innerHTML="";
 	for(let a=0; a<this.mono.vidas; a++){
 		let vida=document.createElement("img");
 		vida.setAttribute("src",Diccionario.imagenes.vida);
@@ -93,6 +108,13 @@ function restear(){
 		this.content_vidas.appendChild(vida);
 	}
 	this.btn_audio.setAttribute("src",Diccionario.imagenes.audio);
+	// Eliminar objetos
+	let objetos=this.content_juego.getElementsByClassName("objeto");
+	let conta=0;
+	while(conta<objetos.length){
+		this.content_juego.removeChild(objetos[0]);
+		conta++;
+	}
 }
 
 function quitarVida(){
@@ -113,8 +135,8 @@ function moverObjeto(objeto){
 		let altura=Number(objeto.style.top.replace("px",""));
 		let izquierda=Number(objeto.style.left.replace("px",""));
 		let izquierda_mono=Number(this.mono.obj.style.left.replace("px",""));
-		objeto.style.top=(altura+1)+"px";
-		if(altura>=limite_abajo){
+		objeto.style.top=(altura+5)+"px";
+		if(altura>=limite_abajo-5){
 			content_juego.removeChild(objeto);
 			clearInterval(intervalo);
 		}else
@@ -123,28 +145,38 @@ function moverObjeto(objeto){
 				this.agregarBanana();
 			}else
 			if(objeto.getAttribute("src")==Diccionario.imagenes.tronco){
-				this.quitarVida();
+				if(!this.quitarVida()){
+					this.mostrarPuntaje();
+				}
 			}
 			content_juego.removeChild(objeto);
 			clearInterval(intervalo);
 		}
-	},10);
+	},50);
 }
 
-function crearObjeto(){
-	let limite_izq=0;
-	let limite_der=window.innerWidth-this.mono.obj.width-10;
-	let tronco=document.createElement("img");
-	tronco.setAttribute("class","objeto");
-	tronco.style.top="0px";
-	tronco.style.left=this.numeroAleatorio(limite_der)+"px";
-	if(this.numeroAleatorio(1)==0){
-		tronco.setAttribute("src",Diccionario.imagenes.banana);
-	}else{
-		tronco.setAttribute("src",Diccionario.imagenes.tronco);
-	}
-	this.content_juego.appendChild(tronco);
-	this.moverObjeto(tronco);
+function crearObjeto(num){
+	let conta=0, intervalo;
+	intervalo=setInterval(()=>{
+		let limite_izq=0;
+		let limite_der=window.innerWidth-this.mono.obj.width-10;
+		let tronco=document.createElement("img");
+		tronco.setAttribute("class","objeto");
+		tronco.style.top="0px";
+		tronco.style.left=this.numeroAleatorio(limite_der)+"px";
+		if(this.numeroAleatorio(2)==0){
+			tronco.setAttribute("src",Diccionario.imagenes.banana);
+		}else{
+			tronco.setAttribute("src",Diccionario.imagenes.tronco);
+		}
+		this.content_juego.appendChild(tronco);
+		this.moverObjeto(tronco);
+		if(conta>=num){
+			this.content_juego.removeChild(tronco);
+			clearInterval(intervalo);
+		}
+		conta++;
+	},this.numeroAleatorio(1500));
 }
 
 function numeroAleatorio(max=0,min=0){
@@ -152,17 +184,40 @@ function numeroAleatorio(max=0,min=0){
 }
 
 function inicio(){
+	this.modal(content_puntaje_obtenido,false);
 	this.modal(content_juego,false);
 	this.modal(content_inicio,true);
+	this.obtenerPuntajes();
+	this.audio.pause();
 }
 
 function jugar(){
 	this.modal(content_juego,true);
 	this.modal(content_inicio,false);
+	this.resetear();
 	this.audio.play();
 	let intervalo;
 	intervalo=setInterval(()=>{
-		this.crearObjeto();
-		this.crearObjeto();
+		this.crearObjeto(this.numeroAleatorio(3,1));
+		if(this.mono.vidas<=0){
+			clearInterval(intervalo);
+		}
 	},1500);
+}
+
+function mostrarPuntaje(){
+	this.modal(content_juego,false);
+	this.modal(content_puntaje_obtenido,true);
+	this.caja_nombre.value="Jugador "+(Number((Puntaje.puntajes()??[]).length)+1);
+	this.puntaje_obtenido.innerHTML=this.mono.bananas;
+	this.audio.pause();
+}
+
+function obtenerPuntajes(){
+	this.content_puntaje.innerHTML="";
+	(Puntaje.puntajes()??[]).forEach((dato)=>{
+		let puntaje=document.createElement("li");
+		puntaje.innerHTML=`${dato.nombre} - ${dato.bananas}`;
+		this.content_puntaje.appendChild(puntaje);
+	});
 }
